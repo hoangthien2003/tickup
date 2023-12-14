60cef58d6715d916ca2d060d565e26d6459979d4
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-12-14 20:22:29 +0700

    add: tickup's landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/shared/components/button";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import HomeLoginButton from "./components/HomeLoginButton";
+import { TickUpLandingPage } from "./components/LandingPageTickup";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen flex items-center justify-center">
-      <h1 className="mr-5">Tickup Page</h1>
-      <HomeLoginButton />
+    <main className="flex items-center w-full h-screen">
+      <TickUpLandingPage />;
     </main>
   );
 }
